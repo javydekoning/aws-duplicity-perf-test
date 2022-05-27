91577ef81e539cd0f00f9ea8fc2dbc0941f132b1
Javy de Koning <javy@amazon.nl>
2022-05-27 21:11:21 +0200

    Initial commit

---
+++ test/backuptesting.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Backuptesting from '../lib/backuptesting-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/backuptesting-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Backuptesting.BackuptestingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
